f3d32411ab8beac8a22315b79a0956474f696e2b
Jan Krüger <jan.krueger.dev@outlook.com>
2022-12-09 21:04:45 +0100

    Added function chebyshev_distance_2d

--- 2022/util/src/lib.rs
+++ 2022/util/src/lib.rs
@@ -1,3 +1,5 @@
+use std::cmp::max;
+
 fn get_input_file() -> &'static str {
     if std::env::var("DEMO").is_ok() {
         "demo_input.txt"
@@ -12,4 +14,11 @@ pub fn get_input_lines() -> Vec<String> {
         .lines()
         .map(|line| line.to_owned())
         .collect()
+}
+
+/// [Chebyshev distance](https://en.wikipedia.org/wiki/Chebyshev_distance)
+///
+/// Distance on a square grid with horizontal, vertical and diagonal movement.
+pub fn chebyshev_distance_2d(p1_x: i32, p1_y: i32, p2_x: i32, p2_y: i32) -> i32 {
+    max((p1_x - p2_x).abs(), (p1_y - p2_y).abs())
 }
