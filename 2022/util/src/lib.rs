c78fdc89eba1560d53ffcd4eb21393cb19f70f8c
Jan Krüger <jan.krueger.dev@outlook.com>
2022-12-17 22:00:06 +0100

    Added function manhatten_distance_2d

--- 2022/util/src/lib.rs
+++ 2022/util/src/lib.rs
@@ -1,4 +1,8 @@
+use priority_queue::PriorityQueue;
 use std::cmp::max;
+use std::cmp::Reverse;
+use std::collections::{HashMap, HashSet};
+use std::hash::Hash;
 
 fn get_input_file() -> &'static str {
     if std::env::var("DEMO").is_ok() {
@@ -21,6 +25,13 @@ pub fn get_input_lines() -> Vec<String> {
 /// Distance on a square grid with horizontal, vertical and diagonal movement.
 pub fn chebyshev_distance_2d(p1_x: i32, p1_y: i32, p2_x: i32, p2_y: i32) -> i32 {
     max((p1_x - p2_x).abs(), (p1_y - p2_y).abs())
+}
+
+/// [Manhattan distance](https://en.wikipedia.org/wiki/Taxicab_geometry)
+///
+/// Distance on a square grid with horizontal and vertical movement.
+pub fn manhatten_distance_2d(p1_x: i32, p1_y: i32, p2_x: i32, p2_y: i32) -> u32 {
+    (p2_x - p1_x).unsigned_abs() + (p2_y - p1_y).unsigned_abs()
 }
 
 pub fn gcd(mut a: u64, mut b: u64) -> u64 {
